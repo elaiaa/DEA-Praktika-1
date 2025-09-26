2b6b8f0b029ce4428079ff3891dc167a0bfb48f8
Gaizka Casales <gaizkacasales13@gmail.com>
2025-09-26 17:41:11 +0200

    Argitalpen con Hashmap

--- DEA_1/src/Klaseak/Argitalpen.java
+++ DEA_1/src/Klaseak/Argitalpen.java
@@ -1,27 +1,22 @@
 package Klaseak;
-import java.util.ArrayList;
+
+import java.util.HashMap;
 import java.util.Iterator;
 
 public class Argitalpen {
 	private String kodea;
 	private String titulu;
-	private ArrayList<Egile> listaegile;
-	private ArrayList<Argitalpen> listaargitalpen;
+	private HashMap<String, Egile> listaegile;
+	private HashMap<String, Argitalpen> listaargitalpen;
 	
 	public Argitalpen(String pKod, String pTit) {
 		this.kodea=pKod;
 		this.titulu=pTit;
-		this.listaargitalpen=new ArrayList<Argitalpen>();
-		this.listaegile=new ArrayList<Egile>();
-	}
-	
-	private Iterator<Argitalpen> getIteratorA(){
-		return this.listaargitalpen.iterator();
-	}
-	private Iterator<Egile> getIteratorE(){
-		return this.listaegile.iterator();
+		this.listaargitalpen=new HashMap<String,Argitalpen>();
+		this.listaegile=new HashMap<String,Egile>();
 	}
 	
 }
 	
+
 
