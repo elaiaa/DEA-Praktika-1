06c5b2c1a92c7a8b2b15dfd0b751491acc1b546a
elaiaa <166693389+elaiaa@users.noreply.github.com>
2025-09-26 17:19:26 +0200

    Update Egile.java

--- DEA_1/src/Klaseak/Egile.java
+++ DEA_1/src/Klaseak/Egile.java
@@ -1,21 +1,33 @@
-package Klaseak;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Egile {
 	private String izena;
 	private String kodea;
-	private ArrayList<Argitalpen> argitalpen;
+	private ArrayList<Argitalpen> argitalpenak;
 	
 	public Egile(String pIzena, String pKodea) {
 		super();
 		this.izena = pIzena;
 		this.kodea = pKodea;
-		this.argitalpen = new ArrayList<Argitalpen>();
+		this.argitalpenak = new ArrayList<Argitalpen>();
 		
 	}
 	 private Iterator<Argitalpen> getIterator(){
-		 return this.argitalpen.iterator();
+		 return this.argitalpenak.iterator();
 	 }
-	
-	
+	 
+	//Egile bat emanda (identifikatzailea), bueltatu bere argitalpenak
+	public void getEgilearenArgitalpenak(Egile pEgile){
+		Iterator<Argitalpen> itr=this.getIterator();
+		while(itr.hasNext()) {
+			Argitalpen a=itr.next();
+			System.out.println(a.getTitulua());
+		}
+		
+		
+	}
 
 }
