b18c8a2ab3c74b16ca31bceea0cf8a3b46b9c274
uporter001 <uporter001@gmail.com>
2025-09-26 18:03:17 +0200

    NO FUINCIONA FitxategienKudeaketa.java

--- DEA_1/src/Klaseak/FitxategienKudeaketa.java
+++ DEA_1/src/Klaseak/FitxategienKudeaketa.java
@@ -1,1 +1,25 @@
+package Klaseak;
 
+import java.io.*;					
+import java.util.Scanner;			//Scanner klasea
+
+
+public class FitxategienKudeaketa {
+public static void main(String[] args) {
+	try {
+		Scanner sc = new Scanner(new FileReader("authors-name-all.txt"));
+		
+		while (sc.hasNextLine()) {
+			int i = sc.nextInt();
+			System.out.println(i);
+		}
+		sc.close();
+	
+	} 
+	catch (FileNotFoundException e) {
+		e.printStackTrace();
+	}
+}
+}
+
+  
