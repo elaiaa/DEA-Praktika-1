1b37f23864447906b1bc98c124344f865b3f934d
elaiaa <166693389+elaiaa@users.noreply.github.com>
2025-09-26 16:50:46 +0200

    Update Egile.java

--- DEA_1/src/Klaseak/Egile.java
+++ DEA_1/src/Klaseak/Egile.java
@@ -1,5 +1,21 @@
 package Klaseak;
 
 public class Egile {
+	private String izena;
+	private String kodea;
+	private ArrayList<Argitalpen> argitalpen;
+	
+	public Egile(String pIzena, String pKodea) {
+		super();
+		this.izena = pIzena;
+		this.kodea = pKodea;
+		this.argitalpen = new ArrayList<Argitalpen>();
+		
+	}
+	 private Iterator<Argitalpen> getIterator(){
+		 return this.argitalpen.iterator();
+	 }
+	
+	
 
 }
