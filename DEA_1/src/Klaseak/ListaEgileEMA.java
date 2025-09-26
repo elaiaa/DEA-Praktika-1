c03e09096391446d32519417906de032c836aa9c
elaiaa <166693389+elaiaa@users.noreply.github.com>
2025-09-26 17:04:24 +0200

    Update ListaEgileEMA.java

--- DEA_1/src/Klaseak/ListaEgileEMA.java
+++ DEA_1/src/Klaseak/ListaEgileEMA.java
@@ -1,1 +1,11 @@
+public class ListaEgileEMA {
+	private ArrayList<Egile> ListaEgile;
+	private static ListaEgileEMA nireListaEgileEMA=null;
+	public static ListaEgileEMA getListaEgileEMA() {
+		if(nireListaEgileEMA==null) {
+			nireListaEgileEMA= new ListaEgileEMA();
+		}
+		return nireListaEgileEMA;
+	}
 
+}
