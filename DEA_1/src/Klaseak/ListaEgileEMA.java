b839bd6f63618b02b3e8eb40dcb73b3715669feb
elaiaa <166693389+elaiaa@users.noreply.github.com>
2025-09-26 17:50:44 +0200

    Update ListaEgileEMA.java

--- DEA_1/src/Klaseak/ListaEgileEMA.java
+++ DEA_1/src/Klaseak/ListaEgileEMA.java
@@ -1,11 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ListaEgileEMA {
-	private ArrayList<Egile> ListaEgile;
+	private HashMap<String,Egile> ListaEgile;
+	private ListaEgileEMA() {new  HashMap<String,Egile>();}
 	private static ListaEgileEMA nireListaEgileEMA=null;
 	public static ListaEgileEMA getListaEgileEMA() {
 		if(nireListaEgileEMA==null) {
 			nireListaEgileEMA= new ListaEgileEMA();
 		}
 		return nireListaEgileEMA;
+	}
+	
+	
+	
+	//Egile bat emanda (identifikatzailea), bueltatu bere argitalpenak
+	public void getEgilearenArgitalpenak(Egile pEgile) {
+		String bilatzekoKodea=pEgile.getkodea();
+		if (ListaEgile.containsKey(bilatzekoKodea)){
+			Egile egilea=ListaEgile.get(bilatzekoKodea);
+			egilea.getEgilearenArgitalpenak();
+		}
 	}
 
 }
