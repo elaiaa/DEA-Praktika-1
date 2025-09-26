ed15b93b7a469e09a6552ddbddbd517299dea4cc
Gaizka Casales <gaizkacasales13@gmail.com>
2025-09-26 17:41:50 +0200

    ListaArgitalpenEMA con HashMap

--- DEA_1/src/Klaseak/ListaArgitalpenEMA.java
+++ DEA_1/src/Klaseak/ListaArgitalpenEMA.java
@@ -2,12 +2,17 @@ package Klaseak;
 import java.util.*;
 
 public class ListaArgitalpenEMA {
-	private ArrayList<Argitalpen> ListaArgitalpen;
+	private HashMap<String, Argitalpen> ListaArgitalpen;
 	private static ListaArgitalpenEMA nireArgitalpenEMA=null;
+	private ListaArgitalpenEMA() {
+		this.ListaArgitalpen=new HashMap<String, Argitalpen>();
+	}
 	public static ListaArgitalpenEMA getListaArgitalpenEMA() {
 		if(nireArgitalpenEMA==null) {
 			nireArgitalpenEMA= new ListaArgitalpenEMA();
 		}
 		return nireArgitalpenEMA;
 	}
+	
+	//metodoak	
 }
