7b5d2fa99fd9ccc308729e9daae1d46f176788a2
elaiaa <166693389+elaiaa@users.noreply.github.com>
2025-10-03 16:51:27 +0200

    Update Egile.java

--- DEA_1/src/Klaseak/Egile.java
+++ DEA_1/src/Klaseak/Egile.java
@@ -1,29 +1,53 @@
-package Klaseak;
-
-
+import java.util.ArrayList;
 import java.util.HashMap;
 
-
-public class Egile {
-	private String izena;
-	private String kodea;
-	private HashMap<String, Argitalpen> argitalpenak;
+public class ListaEgileEMA {
+	private HashMap<String,Egile> ListaEgile;
+	private ListaEgileEMA() {new  HashMap<String,Egile>();}
+	private static ListaEgileEMA nireListaEgileEMA=null;
+	public static ListaEgileEMA getListaEgileEMA() {
+		if(nireListaEgileEMA==null) {
+			nireListaEgileEMA= new ListaEgileEMA();
+		}
+		return nireListaEgileEMA;
+	}
 	
-	public Egile(String pIzena, String pKodea) {
-		super();
-		this.izena = pIzena;
-		this.kodea = pKodea;
-		this.argitalpenak = new HashMap<String, Argitalpen>();
+	
+	
+	//Egile bat emanda (identifikatzailea), bueltatu bere argitalpenak
+	public void getEgilearenArgitalpenak(Egile pEgile) {
+		String bilatzekoKodea=pEgile.getkodea();
+		if (this.aurkitutaEgile(pEgile)){
+			Egile egilea=ListaEgile.get(bilatzekoKodea);
+			egilea.getEgilearenArgitalpenak();
+		}
+	}
+	
+	public void removeEgile(Egile pEgile) {
+		if (this.aurkitutaEgile(pEgile)){ListaEgile.remove(pEgile.getkodea());}
+		ListaEgile.remove(pEgile.getkodea());
+	}
+	public boolean aurkitutaEgile(Egile pEgile) {
+		String bilatzekoKodea=pEgile.getkodea();
+		if (ListaEgile.containsKey(bilatzekoKodea)) {
+			return true;
+		}
+		else
+		{return false;}
+	}
+	
+	public boolean badagoEgilea(String bilatzekoKodea) {
+		if(ListaEgile.containsKey(bilatzekoKodea)) {
+			return true;
+		}
+		else {return false;}
+	}
+	public Egile getEgile(String kodea) {
+		if(this.badagoEgilea(kodea)) {
+			return ListaEgile.get(kodea);
+		}
+		else {return null;}
 		
 	}
-	
-	 
-	//Egile bat emanda (identifikatzailea), bueltatu bere argitalpenak
-	public void getEgilearenArgitalpenak(){
-		for (Argitalpen i : argitalpenak.values()) {
-			  System.out.println(i.getTitulu());
-			}
-	}
 
-	public String getkodea() {return this.kodea;}
 }
