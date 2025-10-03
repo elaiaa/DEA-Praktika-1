762056bd16881585715b23801944ab39bd989b6a
uporter001 <uporter001@gmail.com>
2025-10-03 11:26:33 +0200

    Update FitxategienKudeaketa.java

--- DEA_1/src/Klaseak/FitxategienKudeaketa.java
+++ DEA_1/src/Klaseak/FitxategienKudeaketa.java
@@ -1,29 +1,22 @@
 package Klaseak;
 
 import java.io.*;					
-import java.util.Scanner;			//Scanner klasea
-
+import java.util.Scanner;
 
 public class FitxategienKudeaketa {
-public static void main(String[] args) {
-	//System.out.println(new File("Hola.txt").getAbsolutePath());
-	//File file = new File("Hola.txt");
-	
-	try {
-		String dir = System.getProperty("user.dir");
-		String path = dir + File.separator + "Hola.txt";
-		Scanner sc = new Scanner(new FileReader(path));
+	public static void main(String[] arg) {
+		File nireFitxategia = new File("authors-name-all.txt");
 		
-		while (sc.hasNextLine()) {
-			int i = sc.nextInt();
-			System.out.println(i);
+		try (Scanner nireScanner = new Scanner(nireFitxategia)) {
+			while (nireScanner.hasNextLine()) {
+				String datua = nireScanner.nextLine();
+				System.out.println(datua);
+			}
+			nireScanner.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Errorea gertatu da, ezin daitezte fitxategiak irakurri");
+			e.printStackTrace();
 		}
-		sc.close();
-	
-	} 
-	catch (FileNotFoundException e) {
-		e.printStackTrace();
 	}
-}
 }
   
