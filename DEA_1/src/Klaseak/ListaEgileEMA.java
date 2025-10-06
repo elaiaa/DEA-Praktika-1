31088330716bd4ac1a227f66d42e3d4bec5536a9
uporter001 <uporter001@gmail.com>
2025-10-06 12:13:07 +0200

    Update ListaEgileEMA.java
    egileZerrenda osoa gehitu

--- DEA_1/src/Klaseak/ListaEgileEMA.java
+++ DEA_1/src/Klaseak/ListaEgileEMA.java
@@ -11,6 +11,32 @@ public class ListaEgileEMA {
 		}
 		return nireListaEgileEMA;
 	}
+
+	/*************************USUNE FITXATEGIEN KUDEAKETA***************************/	
+	public void getListaEgile() {
+		HashMap<String, Egile> listaEgile = new HashMap<>();
+		File nireFitxategia = new File("authors-name-all.txt");
+		
+		try (Scanner nireScanner = new Scanner(nireFitxategia)) {
+			while (nireScanner.hasNextLine()) {
+				String lineaOsoa = nireScanner.nextLine();
+				String[] zatitu = lineaOsoa.split(" # ");
+				Egile eg = new Egile(zatitu[1], zatitu[0]);
+				listaEgile.put(zatitu[0], eg);
+			}
+			nireScanner.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Errorea gertatu da, ezin da fitxategia irakurri");
+			e.printStackTrace();
+		}
+		this.ListaEgile = listaEgile;
+	}
+	
+	public Egile getEgile(String pKodea) {
+		Egile eg = this.ListaEgile.get(pKodea);
+		return eg;
+	}
+	/*************************USUNE FITXATEGIEN KUDEAKETA***************************/	
 	
 	
 	
