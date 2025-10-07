5431bcf223e372a2005a7e9c341a35271d030030
uporter001 <uporter001@gmail.com>
2025-10-07 08:03:34 +0200

    Update ListaEgileEMA.java

--- DEA_1/src/Klaseak/ListaEgileEMA.java
+++ DEA_1/src/Klaseak/ListaEgileEMA.java
@@ -1,20 +1,21 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-
+/*************************USUNE FITXATEGIEN KUDEAKETA***************************/	
 public class ListaEgileEMA {
 	private HashMap<String,Egile> ListaEgile;
-	private ListaEgileEMA() {new  HashMap<String,Egile>();}
 	private static ListaEgileEMA nireListaEgileEMA=null;
+	private ListaEgileEMA() {
+		this.ListaEgile = new  HashMap<String,Egile>();
+	}
 	public static ListaEgileEMA getListaEgileEMA() {
 		if(nireListaEgileEMA==null) {
 			nireListaEgileEMA= new ListaEgileEMA();
 		}
 		return nireListaEgileEMA;
 	}
-
-	/*************************USUNE FITXATEGIEN KUDEAKETA***************************/	
+	
+	
 	public void getListaEgile() {
-		HashMap<String, Egile> listaEgile = new HashMap<>();
 		File nireFitxategia = new File("authors-name-all.txt");
 		
 		try (Scanner nireScanner = new Scanner(nireFitxategia)) {
@@ -22,19 +23,38 @@ public class ListaEgileEMA {
 				String lineaOsoa = nireScanner.nextLine();
 				String[] zatitu = lineaOsoa.split(" # ");
 				Egile eg = new Egile(zatitu[1], zatitu[0]);
-				listaEgile.put(zatitu[0], eg);
+				this.ListaEgile.put(zatitu[0], eg);
 			}
 			nireScanner.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("Errorea gertatu da, ezin da fitxategia irakurri");
 			e.printStackTrace();
 		}
-		this.ListaEgile = listaEgile;
 	}
 	
 	public Egile getEgile(String pKodea) {
 		Egile eg = this.ListaEgile.get(pKodea);
 		return eg;
+	}
+	
+	public void getEgileenArgitalpenak() {
+		File nireFitxategia = new File("publications-authors-all-final.txt");
+		ListaArgitalpenEMA argEMA = ListaArgitalpenEMA.getListaArgitalpenEMA();
+		
+		try (Scanner nireScanner = new Scanner(nireFitxategia)) {
+			while (nireScanner.hasNextLine()) {
+				String lineaOsoa = nireScanner.nextLine();
+				String[] zatitu = lineaOsoa.split(" # ");
+				String argKode = zatitu[0];
+				String egiKode = zatitu[1];
+				Argitalpen arg = argEMA.getArgitalpen(argKode);
+				this.ListaEgile.get(egiKode).gehituArgitalpen(argKode, arg);
+			}
+			nireScanner.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Errorea gertatu da, ezin da fitxategia irakurri");
+			e.printStackTrace();
+		}
 	}
 	/*************************USUNE FITXATEGIEN KUDEAKETA***************************/	
 	
